1b7cabb54d5baf01f17eeea3328cc1ff2e244dd0
Shekcon <quangsang9773@gmail.com>
2018-09-05 23:07:07 +0700

    working on it

---
+++ easy/Append_and_Delete.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/append-and-delete/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the appendAndDelete function below.
+def appendAndDelete(s, t, k):
+    pass
+    // working on 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    t = input()
+
+    k = int(input())
+
+    result = appendAndDelete(s, t, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
