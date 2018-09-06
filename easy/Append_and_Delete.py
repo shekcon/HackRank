ad89e0bd2b4d8bf70d21e433f59dc729978f0b3f
Shekcon <quangsang9773@gmail.com>
2018-09-06 20:54:25 +0700

    finish append and delete problem

--- easy/Append_and_Delete.py
+++ easy/Append_and_Delete.py
@@ -9,10 +9,42 @@ import sys
 
 # Complete the appendAndDelete function below.
 def appendAndDelete(s, t, k):
-    pass
-    // working on 
+    """
+    return str
+    """
+    statement = is_same_text(s, t)
+    delete, append = delete_append_time_minium(statement, s, t)
+    print(delete, append)
+    while k - delete - append >= 0:
+        if k - delete - append == 0:
+            return 'Yes'
+        delete += 1
+        """only increment when add not greater than len of array"""
+        append += 1 if append < len(t) else 0
+    return "No"
+    
+
+def delete_append_time_minium(flag, s, t):
+    """
+    cal delete_time_minium and append time minium
+    return int, int
+    """
+    delete_time = len(s) - flag if flag != -1 else len(s)
+    append_time = len(t) - flag if flag != -1 else len(t)
+    return delete_time, append_time
+    
+
+def is_same_text(s, t):
+    """
+    find s start with equal t
+    return int
+    """
+    found = [i for i in range(1,len(t)+1) if s.startswith("".join(t[:i]))]
+    return max(found) if len(found) > 0 else -1
+
+
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
 
     s = input()
 
@@ -22,6 +54,4 @@ if __name__ == '__main__':
 
     result = appendAndDelete(s, t, k)
 
-    fptr.write(result + '\n')
-
-    fptr.close()
+    print(result)
---
+++ easy/Between_Two_Sets.py
@@ -0,0 +1,49 @@
+# https://www.hackerrank.com/challenges/between-two-sets/problem
+
+#!/bin/python3
+    
+import os
+import sys
+
+#
+# Complete the getTotalX function below.
+#
+def getTotalX(a, b):
+    #
+    # Write your code here.
+    #
+    
+
+
+def multiples(number):
+    table_muitiples = []
+    i = 0
+    while number * i < min_b:
+        table_muitiples.append()
+    return  
+
+def get_fator(a, min_b):
+
+    return fators
+
+def is_divide_evenly(b, fator):
+    for number in b:
+        if number % fator != 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(a, b)
+
+    print(result)
