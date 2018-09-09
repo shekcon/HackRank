cff52b77490c2e89f3805b899b26cbadc6804b2c
Shekcon <quangsang9773@gmail.com>
2018-09-09 18:53:43 +0700

    update new solution (easy way)

--- Project Euler/Ex5Smallest_multiple.py
+++ Project Euler/Ex5Smallest_multiple.py
@@ -8,6 +8,20 @@ def dividely(n, number):
         return True
     return dividely(n-1, number) if number % n == 0 else False
 
+"""
+ from functools import reduce
+ from fractions import gcd
+
+ def lcm(a, b):
+     return a * b / gcd(a, b)
+
+ def lcms(*numbers):
+      return reduce(lcm, numbers)
+
+ def gcds(*numbers):
+      return reduce(gcd, numbers)
+"""
+
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
@@ -15,3 +29,7 @@ for a0 in range(t):
     while dividely(n, i) is False:
         i += 1
     print(i)
+    """can use this way
+    print(reduce(lcm,[number for number in range(1, n+1)]))
+
+    """
