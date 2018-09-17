0801000e2c59ff09a3dc26ebffdbf7a0f861af77
Shekcon <quangsang9773@gmail.com>
2018-09-17 22:57:43 +0700

    Jumping on the Clouds Revisited completed

---
+++ easy/Jumping_on_the_Clouds_Revisited.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    sub_energy = [ 3 if c[i] == 1 else 1 for i in range(0, len(c), k) ]
+    return 100 - sum(sub_energy)    
+
+if __name__ == '__main__':
+
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    print(str(result))
+
