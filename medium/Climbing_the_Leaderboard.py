a3fc322433e1f7ef3c477cc19ec38d4d62522b2e
Shekcon <quangsang9773@gmail.com>
2018-09-18 22:10:04 +0700

    use binary search for this problem

--- medium/Climbing_the_Leaderboard.py
+++ medium/Climbing_the_Leaderboard.py
@@ -9,8 +9,7 @@ import sys
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
     scores = list(sorted(set(scores), key=int, reverse=True))
-    climb_record = [ ranked_now(ponit, scores) for ponit in alice]
-    return climb_record
+    return [ranked_search(scores, ponit)+1 for ponit in alice]
 
 
 def ranked_now(score, leader):
@@ -20,7 +19,28 @@ def ranked_now(score, leader):
             break
     return len(leader)+1 if ranked is None else ranked 
 
+# base on binary search algorithms
+# solution for exercise on hackrank
+def ranked_search(content, score):
+    first = 0
+    last = len(content) - 1
+    # is score bigger than rank 1
+    if score >= content[first]:
+        return first
+    elif score <= content[last]:
+        # is item equal lowest ranked or less than
+        return last if score == content[last] else last+1
+    while first<=last:
+        midpoint = (first + last)//2
+        if content[midpoint] == score:
+            return midpoint
+        else:
+            if score > content[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
 
+    return midpoint if score == content[midpoint] else midpoint +1
 
 if __name__ == '__main__':
 
